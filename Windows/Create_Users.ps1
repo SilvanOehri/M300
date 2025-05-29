bef33953359c1c1edb9c2c4975ef3330cc77b0df
Silvan Oehri <silvan.oehri@lgt.com>
2025-05-29 14:50:22 +0200

    fix: user creation

--- Windows/Create_Users.ps1
+++ Windows/Create_Users.ps1
@@ -1,118 +1,112 @@
 <#
 .SYNOPSIS
-    Creates predefined AD users for the osWDo.local domain based on a fixed list.
+    Erstellt eindeutige AD-Benutzer für osWDo.local. Existierende Benutzer werden vorher gelöscht.
 
 .NOTES
-    Author : Dein Name
-    Date   : 2025-05-28
+    Autor : Silvan Oehri
+    Datum : 2025-05-29
 #>
 
-# ==== CONFIGURATION ====
-$domain = "osWDo.local"
-$baseDN = "OU=osWDo,DC=osWDo,DC=local"
+$domain   = "osWDo.local"
+$baseDN   = "OU=osWDo,DC=osWDo,DC=local"
 $password = ConvertTo-SecureString "Start123!" -AsPlainText -Force
 
-# ==== USER DEFINITION ====
 $users = @(
-    # GF
-    @{ FirstName = "Lukas"; LastName = "Meyer"; Department = "GF" },
-
-    # Sekretariat (8)
-    @{ FirstName = "Anna"; LastName = "Keller"; Department = "Sekretariat" },
-    @{ FirstName = "Mia"; LastName = "Huber"; Department = "Sekretariat" },
-    @{ FirstName = "Noah"; LastName = "Müller"; Department = "Sekretariat" },
-    @{ FirstName = "Lea"; LastName = "Schmid"; Department = "Sekretariat" },
-    @{ FirstName = "Ben"; LastName = "Weber"; Department = "Sekretariat" },
-    @{ FirstName = "Nina"; LastName = "Meier"; Department = "Sekretariat" },
-    @{ FirstName = "Tom"; LastName = "Frei"; Department = "Sekretariat" },
-    @{ FirstName = "Sara"; LastName = "Graf"; Department = "Sekretariat" },
-
-    # Technik (12)
-    @{ FirstName = "Max"; LastName = "Baumgartner"; Department = "Technik" },
-    @{ FirstName = "Lukas"; LastName = "Keller"; Department = "Technik" },
-    @{ FirstName = "Anna"; LastName = "Huber"; Department = "Technik" },
-    @{ FirstName = "Mia"; LastName = "Müller"; Department = "Technik" },
-    @{ FirstName = "Noah"; LastName = "Schmid"; Department = "Technik" },
-    @{ FirstName = "Lea"; LastName = "Weber"; Department = "Technik" },
-    @{ FirstName = "Ben"; LastName = "Meier"; Department = "Technik" },
-    @{ FirstName = "Nina"; LastName = "Frei"; Department = "Technik" },
-    @{ FirstName = "Tom"; LastName = "Graf"; Department = "Technik" },
-    @{ FirstName = "Sara"; LastName = "Baumgartner"; Department = "Technik" },
-    @{ FirstName = "Max"; LastName = "Meyer"; Department = "Technik" },
-    @{ FirstName = "Lukas"; LastName = "Huber"; Department = "Technik" },
-
-    # Entwicklung (19)
-    @{ FirstName = "Anna"; LastName = "Müller"; Department = "Entwicklung" },
-    @{ FirstName = "Mia"; LastName = "Schmid"; Department = "Entwicklung" },
-    @{ FirstName = "Noah"; LastName = "Weber"; Department = "Entwicklung" },
-    @{ FirstName = "Lea"; LastName = "Meier"; Department = "Entwicklung" },
-    @{ FirstName = "Ben"; LastName = "Frei"; Department = "Entwicklung" },
-    @{ FirstName = "Nina"; LastName = "Graf"; Department = "Entwicklung" },
-    @{ FirstName = "Tom"; LastName = "Baumgartner"; Department = "Entwicklung" },
-    @{ FirstName = "Sara"; LastName = "Meyer"; Department = "Entwicklung" },
-    @{ FirstName = "Max"; LastName = "Keller"; Department = "Entwicklung" },
-    @{ FirstName = "Lukas"; LastName = "Müller"; Department = "Entwicklung" },
-    @{ FirstName = "Anna"; LastName = "Schmid"; Department = "Entwicklung" },
-    @{ FirstName = "Mia"; LastName = "Weber"; Department = "Entwicklung" },
-    @{ FirstName = "Noah"; LastName = "Meier"; Department = "Entwicklung" },
-    @{ FirstName = "Lea"; LastName = "Frei"; Department = "Entwicklung" },
-    @{ FirstName = "Ben"; LastName = "Graf"; Department = "Entwicklung" },
-    @{ FirstName = "Nina"; LastName = "Baumgartner"; Department = "Entwicklung" },
-    @{ FirstName = "Tom"; LastName = "Meyer"; Department = "Entwicklung" },
-    @{ FirstName = "Sara"; LastName = "Keller"; Department = "Entwicklung" },
-    @{ FirstName = "Max"; LastName = "Huber"; Department = "Entwicklung" },
-
-    # Schulung (10)
-    @{ FirstName = "Lukas"; LastName = "Schmid"; Department = "Schulung" },
-    @{ FirstName = "Anna"; LastName = "Weber"; Department = "Schulung" },
-    @{ FirstName = "Mia"; LastName = "Meier"; Department = "Schulung" },
-    @{ FirstName = "Noah"; LastName = "Frei"; Department = "Schulung" },
-    @{ FirstName = "Lea"; LastName = "Graf"; Department = "Schulung" },
-    @{ FirstName = "Ben"; LastName = "Baumgartner"; Department = "Schulung" },
-    @{ FirstName = "Nina"; LastName = "Keller"; Department = "Schulung" },
-    @{ FirstName = "Tom"; LastName = "Huber"; Department = "Schulung" },
-    @{ FirstName = "Sara"; LastName = "Schmid"; Department = "Schulung" },
-    @{ FirstName = "Max"; LastName = "Weber"; Department = "Schulung" },
-
-    # Kurse (5)
-    @{ FirstName = "Lukas"; LastName = "Meier"; Department = "Kurse" },
-    @{ FirstName = "Anna"; LastName = "Frei"; Department = "Kurse" },
-    @{ FirstName = "Mia"; LastName = "Graf"; Department = "Kurse" },
-    @{ FirstName = "Noah"; LastName = "Baumgartner"; Department = "Kurse" },
-    @{ FirstName = "Lea"; LastName = "Keller"; Department = "Kurse" },
-
-    # Produktion (25)
-    @{ FirstName = "Ben"; LastName = "Huber"; Department = "Produktion" },
-    @{ FirstName = "Nina"; LastName = "Schmid"; Department = "Produktion" },
-    @{ FirstName = "Tom"; LastName = "Weber"; Department = "Produktion" },
-    @{ FirstName = "Sara"; LastName = "Meier"; Department = "Produktion" },
-    @{ FirstName = "Max"; LastName = "Frei"; Department = "Produktion" },
-    @{ FirstName = "Lukas"; LastName = "Graf"; Department = "Produktion" },
-    @{ FirstName = "Anna"; LastName = "Baumgartner"; Department = "Produktion" },
-    @{ FirstName = "Mia"; LastName = "Keller"; Department = "Produktion" },
-    @{ FirstName = "Noah"; LastName = "Huber"; Department = "Produktion" },
-    @{ FirstName = "Lea"; LastName = "Schmid"; Department = "Produktion" },
-    @{ FirstName = "Ben"; LastName = "Weber"; Department = "Produktion" },
-    @{ FirstName = "Nina"; LastName = "Meier"; Department = "Produktion" },
-    @{ FirstName = "Tom"; LastName = "Frei"; Department = "Produktion" },
-    @{ FirstName = "Sara"; LastName = "Graf"; Department = "Produktion" },
-    @{ FirstName = "Max"; LastName = "Baumgartner"; Department = "Produktion" },
-    @{ FirstName = "Lukas"; LastName = "Keller"; Department = "Produktion" },
-    @{ FirstName = "Anna"; LastName = "Huber"; Department = "Produktion" },
-    @{ FirstName = "Mia"; LastName = "Müller"; Department = "Produktion" },
-    @{ FirstName = "Noah"; LastName = "Schmid"; Department = "Produktion" },
-    @{ FirstName = "Lea"; LastName = "Weber"; Department = "Produktion" },
-    @{ FirstName = "Ben"; LastName = "Meier"; Department = "Produktion" },
-    @{ FirstName = "Nina"; LastName = "Frei"; Department = "Produktion" },
-    @{ FirstName = "Tom"; LastName = "Graf"; Department = "Produktion" },
-    @{ FirstName = "Sara"; LastName = "Baumgartner"; Department = "Produktion" },
-    @{ FirstName = "Max"; LastName = "Meyer"; Department = "Produktion" }
+    @{"FirstName" = "Lisa"; "LastName" = "Roth"; "Department" = "GF"},
+    @{"FirstName" = "Nina"; "LastName" = "Maurer"; "Department" = "Sekretariat"},
+    @{"FirstName" = "Moritz"; "LastName" = "Zimmermann"; "Department" = "Sekretariat"},
+    @{"FirstName" = "Nina"; "LastName" = "Roth"; "Department" = "Sekretariat"},
+    @{"FirstName" = "Felix"; "LastName" = "Gerber"; "Department" = "Sekretariat"},
+    @{"FirstName" = "Clara"; "LastName" = "Graf"; "Department" = "Sekretariat"},
+    @{"FirstName" = "Timo"; "LastName" = "Hofer"; "Department" = "Sekretariat"},
+    @{"FirstName" = "David"; "LastName" = "Haas"; "Department" = "Sekretariat"},
+    @{"FirstName" = "Timo"; "LastName" = "Huber"; "Department" = "Sekretariat"},
+    @{"FirstName" = "Jonas"; "LastName" = "Keller"; "Department" = "Technik"},
+    @{"FirstName" = "Marie"; "LastName" = "Graf"; "Department" = "Technik"},
+    @{"FirstName" = "Paul"; "LastName" = "Weber"; "Department" = "Technik"},
+    @{"FirstName" = "Sara"; "LastName" = "Peter"; "Department" = "Technik"},
+    @{"FirstName" = "Paul"; "LastName" = "Schuster"; "Department" = "Technik"},
+    @{"FirstName" = "Laura"; "LastName" = "Meyer"; "Department" = "Technik"},
+    @{"FirstName" = "Lena"; "LastName" = "Schneider"; "Department" = "Technik"},
+    @{"FirstName" = "Anna"; "LastName" = "Vogel"; "Department" = "Technik"},
+    @{"FirstName" = "Felix"; "LastName" = "Hofer"; "Department" = "Technik"},
+    @{"FirstName" = "Max"; "LastName" = "Haas"; "Department" = "Technik"},
+    @{"FirstName" = "Jana"; "LastName" = "Huber"; "Department" = "Technik"},
+    @{"FirstName" = "Tom"; "LastName" = "Mueller"; "Department" = "Technik"},
+    @{"FirstName" = "Mia"; "LastName" = "Beck"; "Department" = "Entwicklung"},
+    @{"FirstName" = "Erik"; "LastName" = "Bader"; "Department" = "Entwicklung"},
+    @{"FirstName" = "Laura"; "LastName" = "Bader"; "Department" = "Entwicklung"},
+    @{"FirstName" = "Lukas"; "LastName" = "Schuster"; "Department" = "Entwicklung"},
+    @{"FirstName" = "Emma"; "LastName" = "Kuhn"; "Department" = "Entwicklung"},
+    @{"FirstName" = "Marie"; "LastName" = "Frei"; "Department" = "Entwicklung"},
+    @{"FirstName" = "Johanna"; "LastName" = "Huber"; "Department" = "Entwicklung"},
+    @{"FirstName" = "Jana"; "LastName" = "Zimmermann"; "Department" = "Entwicklung"},
+    @{"FirstName" = "Timo"; "LastName" = "Suter"; "Department" = "Entwicklung"},
+    @{"FirstName" = "Tim"; "LastName" = "Kuhn"; "Department" = "Entwicklung"},
+    @{"FirstName" = "Moritz"; "LastName" = "Hofer"; "Department" = "Entwicklung"},
+    @{"FirstName" = "Nina"; "LastName" = "Suter"; "Department" = "Entwicklung"},
+    @{"FirstName" = "Marie"; "LastName" = "Mueller"; "Department" = "Entwicklung"},
+    @{"FirstName" = "Paul"; "LastName" = "Roth"; "Department" = "Entwicklung"},
+    @{"FirstName" = "Lukas"; "LastName" = "Arnold"; "Department" = "Entwicklung"},
+    @{"FirstName" = "Emma"; "LastName" = "Schmid"; "Department" = "Entwicklung"},
+    @{"FirstName" = "Nina"; "LastName" = "Graf"; "Department" = "Entwicklung"},
+    @{"FirstName" = "Tom"; "LastName" = "Schmid"; "Department" = "Entwicklung"},
+    @{"FirstName" = "Anna"; "LastName" = "Suter"; "Department" = "Entwicklung"},
+    @{"FirstName" = "Mia"; "LastName" = "Schmid"; "Department" = "Schulung"},
+    @{"FirstName" = "David"; "LastName" = "Wagner"; "Department" = "Schulung"},
+    @{"FirstName" = "Emma"; "LastName" = "Wirth"; "Department" = "Schulung"},
+    @{"FirstName" = "Jan"; "LastName" = "Vogel"; "Department" = "Schulung"},
+    @{"FirstName" = "Marie"; "LastName" = "Meyer"; "Department" = "Schulung"},
+    @{"FirstName" = "Tom"; "LastName" = "Wagner"; "Department" = "Schulung"},
+    @{"FirstName" = "Emma"; "LastName" = "Vogel"; "Department" = "Schulung"},
+    @{"FirstName" = "Jonas"; "LastName" = "Zimmermann"; "Department" = "Schulung"},
+    @{"FirstName" = "Erik"; "LastName" = "Beck"; "Department" = "Schulung"},
+    @{"FirstName" = "Paul"; "LastName" = "Hofer"; "Department" = "Schulung"},
+    @{"FirstName" = "Paul"; "LastName" = "Baumgartner"; "Department" = "Kurse"},
+    @{"FirstName" = "Simon"; "LastName" = "Arnold"; "Department" = "Kurse"},
+    @{"FirstName" = "Tim"; "LastName" = "Baumgartner"; "Department" = "Kurse"},
+    @{"FirstName" = "Lea"; "LastName" = "Brunner"; "Department" = "Kurse"},
+    @{"FirstName" = "Erik"; "LastName" = "Zimmermann"; "Department" = "Kurse"},
+    @{"FirstName" = "Max"; "LastName" = "Vogel"; "Department" = "Produktion"},
+    @{"FirstName" = "Paul"; "LastName" = "Zimmermann"; "Department" = "Produktion"},
+    @{"FirstName" = "Simon"; "LastName" = "Meier"; "Department" = "Produktion"},
+    @{"FirstName" = "Johanna"; "LastName" = "Beck"; "Department" = "Produktion"},
+    @{"FirstName" = "Emma"; "LastName" = "Ziegler"; "Department" = "Produktion"},
+    @{"FirstName" = "Laura"; "LastName" = "Huber"; "Department" = "Produktion"},
+    @{"FirstName" = "Noah"; "LastName" = "Vogel"; "Department" = "Produktion"},
+    @{"FirstName" = "Mia"; "LastName" = "Graf"; "Department" = "Produktion"},
+    @{"FirstName" = "Clara"; "LastName" = "Maurer"; "Department" = "Produktion"},
+    @{"FirstName" = "Lukas"; "LastName" = "Keller"; "Department" = "Produktion"},
+    @{"FirstName" = "Felix"; "LastName" = "Brunner"; "Department" = "Produktion"},
+    @{"FirstName" = "Tom"; "LastName" = "Roth"; "Department" = "Produktion"},
+    @{"FirstName" = "Lena"; "LastName" = "Haas"; "Department" = "Produktion"},
+    @{"FirstName" = "Laura"; "LastName" = "Hofer"; "Department" = "Produktion"},
+    @{"FirstName" = "Tom"; "LastName" = "Hofer"; "Department" = "Produktion"},
+    @{"FirstName" = "Sara"; "LastName" = "Frei"; "Department" = "Produktion"},
+    @{"FirstName" = "Timo"; "LastName" = "Vogel"; "Department" = "Produktion"},
+    @{"FirstName" = "Clara"; "LastName" = "Schneider"; "Department" = "Produktion"},
+    @{"FirstName" = "Tim"; "LastName" = "Haas"; "Department" = "Produktion"},
+    @{"FirstName" = "Clara"; "LastName" = "Baumgartner"; "Department" = "Produktion"},
+    @{"FirstName" = "Paul"; "LastName" = "Wirth"; "Department" = "Produktion"},
+    @{"FirstName" = "Ben"; "LastName" = "Graf"; "Department" = "Produktion"},
+    @{"FirstName" = "Anna"; "LastName" = "Zimmermann"; "Department" = "Produktion"},
+    @{"FirstName" = "Johanna"; "LastName" = "Frei"; "Department" = "Produktion"},
+    @{"FirstName" = "Anna"; "LastName" = "Arnold"; "Department" = "Produktion"}
 )
 
-# ==== CREATE USERS ====
 foreach ($user in $users) {
     $username = ("{0}.{1}" -f $user.FirstName, $user.LastName).ToLower()
-    $ouPath = "OU=$($user.Department),OU=Users,$baseDN"
+    $ouPath   = "OU=$($user.Department),OU=Users,$baseDN"
+
+    $existing = Get-ADUser -Filter { SamAccountName -eq $username } -ErrorAction SilentlyContinue
+    if ($existing) {
+        try {
+            Remove-ADUser -Identity $existing -Confirm:$false
+            Write-Host "❌ Alter Benutzer gelöscht: $username"
+        } catch {
+            Write-Host "⚠️ Fehler beim Löschen von $username: $($_.Exception.Message)" -ForegroundColor Yellow
+        }
+    }
 
     try {
         New-ADUser `
@@ -126,10 +120,8 @@ foreach ($user in $users) {
             -AccountPassword $password `
             -Enabled $true `
             -ChangePasswordAtLogon $false
-
-        Write-Host "Created user: $username in $($user.Department)"
-    }
-    catch {
-        Write-Host "Error creating $username: $($_.Exception.Message)" -ForegroundColor Red
+        Write-Host "✔ Benutzer erstellt: $username in $($user.Department)"
+    } catch {
+        Write-Host "❌ Fehler bei $username: $($_.Exception.Message)" -ForegroundColor Red
     }
 }
